708d2036afc62bad6a9aa1e233cb2dfdcd489990
James Abney <james.abney@gmail.com>
2021-07-21 09:10:50 -0500

    Update text

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>{result ?? 'calculating...'}</Text>
+        <Text>{result ?? 'calculating pi...'}</Text>
       </View>
     </SafeAreaView>
   )
