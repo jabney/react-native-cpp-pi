1bdfb878b933943d30e6b2521777eea0c7142715
James Abney <james.abney@gmail.com>
2021-07-21 12:50:02 -0500

    Add cpp-adapter.cpp

---
+++ android/cpp-adapter.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Marius Reimer on 2019-08-10.
+//
+
+#include <iostream>
+#include <jni.h>
+#include <android/log.h>
+#include "pi.h"
+
+using namespace std;
+
+extern "C" JNIEXPORT jdouble JNICALL
+Java_com_cpppi_calculatepi_CalculatePiModule_00024Companion_calculate(JNIEnv *env, jobject type, jlong iterations)
+{
+    return cppmodule::calculate(static_cast<uint64_t>(iterations));
+}
