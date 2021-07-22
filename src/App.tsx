7e8596827ac8ad36adc2adef4504d8de3e21d875
James Abney <james.abney@gmail.com>
2021-07-22 16:55:56 -0500

    Restructure for concurrency output

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,34 @@ import { SafeAreaView, Text, View } from 'react-native'
 import { CalculatePi } from './CalculatePi'
 
 const App: React.FC = () => {
+  const [concurrency, setConcurrency] = useState(0)
   const [result, setResult] = useState<number | null>(null)
 
+  useEffect(
+    () =>
+      void CalculatePi.concurrency().then(
+        // Avoid flashing the concurrency loading message too quickly.
+        (result) => void setTimeout(() => setConcurrency(result), 1000),
+      ),
+    [],
+  )
+
   useEffect(() => {
-    setTimeout(() => {
+    if (concurrency > 0) {
       CalculatePi.calculate(1e10).then(setResult)
-    }, 0)
-  }, [])
+    }
+  }, [concurrency])
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>{result ?? 'calculating pi...'}</Text>
+        {concurrency > 0 ? (
+          <Text>
+            {result ?? `Calculating pi using ${concurrency} threads...`}
+          </Text>
+        ) : (
+          <Text>Checking concurrency...</Text>
+        )}
       </View>
     </SafeAreaView>
   )
